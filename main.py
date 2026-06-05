b55557d97d331d3797b7aea085534dcabcb02f77
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-05 22:54:15 +0530

    feat: add LangChain tool for Tavily-powered web search

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from tavily import TavilyClient
+from langchain.tools import tool
 import os
 
 tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
-result = tavily_client.search(query="What is the price of Bitcoin?")
+@tool
+def surfInternet(query:str):
+    """"USe this tool for getting the latest information from the internet"""
 
-print(result)
+    result = tavily_client.search(query=query)
+
+    print(result)
+
+
+surfInternet.invoke({"query":"Kolkata News"})
